adbf9d52f36d66a4d8e4b56c91a7258149de334b
Vincent Dupont <vincent.touffi@gmail.com>
2013-10-23 15:04:23 +0200

    Uodate float implementation
    + update indentation

--- libraries/SevenSeg/SevenSeg.cpp
+++ libraries/SevenSeg/SevenSeg.cpp
@@ -8,162 +8,162 @@
 
 SevenSeg::SevenSeg() {
 
-  SPI.begin();
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
-  
-  m_dot = 0;
-  m_isChar = 0;
-  m_isNeg = 0;
-  
+	SPI.begin();
+	SPI.setBitOrder(MSBFIRST);
+	SPI.setDataMode(SPI_MODE0);
+
+	m_dot = 0;
+	m_isChar = 0;
+	m_isNeg = 0;
+
 }
 
 int SevenSeg::printValueSync() {
-  
-  int value;
-  unsigned int val;
-  int i = 0;
 
-  if (m_isChar) {
-    // Char / string
-    val = m_value.tab[0];
-    while (val != 0 && i < DISP_SIZE) {
-      spiSendAtIndex(val & 0xFF, i);
-      val = m_value.tab[++i];
-    }
-  } else {
-    // Numeric value
-    value = m_value.binVal;
-    if (m_isNeg)
-      value = -value;
-    while (value > 0 && i < DISP_SIZE) {
-      
-      val = seven_seg[value % 10];
-      if (m_dot & 1 << i)
-        val &= ~0x80;
-    
-      spiSendAtIndex(val, i);      
-      
-      value /= 10;
-      i++;
-    }
-    if (m_isNeg)
-      spiSendAtIndex(seven_seg[NB_CHAR-1], i); // Carret is the last one
-  }
-  
-  return 0;
-  
+	int value;
+	unsigned int val;
+	int i = 0;
+
+	if (m_isChar) {
+		// Char / string
+		val = m_value.tab[0];
+		while (val != 0 && i < DISP_SIZE) {
+			spiSendAtIndex(val & 0xFF, i);
+			val = m_value.tab[++i];
+		}
+	} else {
+		// Numeric value
+		value = m_value.binVal;
+		if (m_isNeg)
+			value = -value;
+		while (value > 0 && i < DISP_SIZE) {
+
+			val = seven_seg[value % 10];
+			if (m_dot & 1 << i)
+				val &= ~0x80;
+
+			spiSendAtIndex(val, i);
+
+			value /= 10;
+			i++;
+		}
+		if (m_isNeg)
+			spiSendAtIndex(seven_seg[NB_CHAR-1], i); // Carret is the last one
+	}
+
+	return 0;
+
 }
 
 void SevenSeg::spiSendAtIndex(unsigned int val, int index){
-  
-  digitalWrite(SS, LOW);
-     
-  SPI.transfer((0x1 << (DISP_SIZE - 1 - index)) & 0xFF);
-  SPI.transfer(val & 0xFF);
-      
-  digitalWrite(SS, HIGH);
-      
+
+	digitalWrite(SS, LOW);
+
+	SPI.transfer((0x1 << (DISP_SIZE - 1 - index)) & 0xFF);
+	SPI.transfer(val & 0xFF);
+
+	digitalWrite(SS, HIGH);
+
 }
 
 int SevenSeg::setValue(long value) {
-  
-  m_isChar = 0;
-  
-  if (value < 0)
-    m_isNeg = 1;
-  else
-    m_isNeg = 0;
 
-  m_value.binVal = value;
-  return value;
+	m_isChar = 0;
+
+	if (value < 0)
+		m_isNeg = 1;
+	else
+		m_isNeg = 0;
+
+	m_value.binVal = value;
+	return value;
 
 }
 
 int SevenSeg::setValue(char value) {
 
-  int v;
-  
-  m_isChar = 1;
-  
-  v = charToVal(value);
-  m_value.binVal = v >= 0 ? v : 0;
-  return m_value.binVal;
+	int v;
+
+	m_isChar = 1;
+
+	v = charToVal(value);
+	m_value.binVal = v >= 0 ? v : 0;
+	return m_value.binVal;
 
 }
 
 int SevenSeg::setValue(char str[]) {
- 
-  int i = 0, j = 0;
-  int v = 0;
-  char c;
-  char dot = 0xFF; // needed to mask the dot value
-  
-  m_isChar = 1;  
-  m_value.binVal = 0;
-  
-  if (str == NULL)
-    return -1;
-  
-  c =str[0];  
-  // Look for the end of the string (max 2 DISP_SIZE if there is some dots)
-  while (c != '\0' && i < 2 * DISP_SIZE) {
-    c = str[++i];
-  }
-  // Write char from last to first because last on will be sent first
-  j = 0;
-  for (i--; i >= 0 && j < DISP_SIZE; i--) {
-    c = str[i];
-    if (c != '.') {
-      v = charToVal(c);
-      v = v >= 0 ? v : 0;
-      m_value.tab[j] = v & dot;
-      dot = 0xFF;
-      j++;
-    } else {
-      dot = 0x7F; // 0x7F is the dot value in seven_seg
-    }
-  }
-  
-  return m_value.binVal;
-  
+
+	int i = 0, j = 0;
+	int v = 0;
+	char c;
+	char dot = 0xFF; // needed to mask the dot value
+
+	m_isChar = 1;
+	m_value.binVal = 0;
+
+	if (str == NULL)
+		return -1;
+
+	c =str[0];
+	// Look for the end of the string (max 2 DISP_SIZE if there is some dots)
+	while (c != '\0' && i < 2 * DISP_SIZE) {
+		c = str[++i];
+	}
+	// Write char from last to first because last on will be sent first
+	j = 0;
+	for (i--; i >= 0 && j < DISP_SIZE; i--) {
+		c = str[i];
+		if (c != '.') {
+			v = charToVal(c);
+			v = v >= 0 ? v : 0;
+			m_value.tab[j] = v & dot;
+			dot = 0xFF;
+			j++;
+		} else {
+			dot = 0x7F; // 0x7F is the dot value in seven_seg
+		}
+	}
+
+	return m_value.binVal;
+
 }
 
 int SevenSeg::setValue(float value, int nbDec) {
-   
-  m_isChar = 0;
-  
-  if (value < 0.0) {
-    m_isNeg = 1;
-    value = -value;
-  }
-  
-  int i;
-  for (i = 0; i < nbDec; i++) {
-    value *= 10;
-  }
-  m_value.binVal = (unsigned long) value;
-  m_dot = nbDec == 0 ? 0 : 1 << (nbDec + 1);
-  
-  return m_value.binVal;
-  
+
+	m_isChar = 0;
+
+	if (value < 0.0) {
+		m_isNeg = 1;
+		value = -value;
+	}
+
+	int i;
+	for (i = 0; i < nbDec; i++) {
+		value *= 10;
+	}
+	m_value.binVal = (unsigned long) value;
+	m_dot = nbDec == 0 ? 0 : 1 << (nbDec);
+
+	return m_value.binVal;
+
 }
 
 int SevenSeg::charToVal(char c) {
- 
-  int i;
-  for (i = 0; i < NB_CHAR; i++) {
-    if (tab_char[i] == c)
-      return seven_seg[i];
-  }
-  return -1;
-  
+
+	int i;
+	for (i = 0; i < NB_CHAR; i++) {
+		if (tab_char[i] == c)
+			return seven_seg[i];
+	}
+	return -1;
+
 }
 
 byte SevenSeg::setDots(byte dots) {
-  
-  m_dot = dots;
-  return m_dot;
-  
+
+	m_dot = dots;
+	return m_dot;
+
 }
 
--- libraries/SevenSeg/SevenSeg.h
+++ libraries/SevenSeg/SevenSeg.h
@@ -1,15 +1,15 @@
 /*
  * SevenSeg.h
- * 
+ *
  * Drive the 7segments display through SPI bus
  *
  * Display works with a two-byte transfer.
  * First Byte sent: index of display
  * Second byte sent: value to display at index
  */
- 
+
 #ifndef SEVEN_SEG_H
-#define SEVEN_SEG_H 
+#define SEVEN_SEG_H
 
 #include "Arduino.h"
 
@@ -23,65 +23,65 @@
 //   3---  7.
 // A bit=0 means the led is on (the last one is the dot).
 const unsigned int seven_seg[NB_CHAR] = {
-  0xC0, // 0
-  0xF9, // 1
-  0xA4, // 2
-  0xB0, // 3
-  0x99, // 4
-  0x92, // 5
-  0x82, // 6
-  0xF8, // 7
-  0x80, // 8
-  0x90, // 9
-  0x88, // A
-  0x83, // b
-  0xC6, // C
-  0xA1, // d
-  0x86, // E
-  0x8E, // F
-  0x82, // G (== 6)
-  0x89, // H
-  0xF9, // I (== 1)
-  0xE1, // J
-  0xC7, // L
-  0xC0, // O (== 0)
-  0x8C, // P
-  0x92, // S (== 5)
-  0xC1, // U
-  0x7F, // .
-  0xFF, // space
-  0xBF, // - (CARRET MUST BE THE LAST ONE IN THIS TAB)
+	0xC0, // 0
+	0xF9, // 1
+	0xA4, // 2
+	0xB0, // 3
+	0x99, // 4
+	0x92, // 5
+	0x82, // 6
+	0xF8, // 7
+	0x80, // 8
+	0x90, // 9
+	0x88, // A
+	0x83, // b
+	0xC6, // C
+	0xA1, // d
+	0x86, // E
+	0x8E, // F
+	0x82, // G (== 6)
+	0x89, // H
+	0xF9, // I (== 1)
+	0xE1, // J
+	0xC7, // L
+	0xC0, // O (== 0)
+	0x8C, // P
+	0x92, // S (== 5)
+	0xC1, // U
+	0x7F, // .
+	0xFF, // space
+	0xBF, // - (CARRET MUST BE THE LAST ONE IN THIS TAB)
 };
 
 const char tab_char[NB_CHAR+1] = "0123456789ABCDEFGHIJLOPSU. -";
 
 union spiValue {
-  unsigned char tab[DISP_SIZE];
-  unsigned long long binVal;
+	unsigned char tab[DISP_SIZE];
+	unsigned long long binVal;
 };
 
 class SevenSeg {
- 
-  public:
-    SevenSeg();
-    int printValueSync();
-    int setValue(long value);
-    int setValue(char value);
-    int setValue(float value, int nbDec = 1);
-    int setValue(char str[]);
-    
-    byte setDots(byte dots);
-    
-  private:
-    spiValue m_value;
-    byte m_dot;
-    
-    int m_isChar;
-    int m_isNeg;
-    
-    int charToVal(char c);
-    void spiSendAtIndex(unsigned int val, int index);
-  
+
+public:
+	SevenSeg();
+	int printValueSync();
+	int setValue(long value);
+	int setValue(char value);
+	int setValue(float value, int nbDec = 1);
+	int setValue(char str[]);
+
+	byte setDots(byte dots);
+
+private:
+	spiValue m_value;
+	byte m_dot;
+
+	int m_isChar;
+	int m_isNeg;
+
+	int charToVal(char c);
+	void spiSendAtIndex(unsigned int val, int index);
+
 };
 
 #endif // SEVEN_SEG_H
