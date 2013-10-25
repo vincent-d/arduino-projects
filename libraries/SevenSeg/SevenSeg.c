7350eb8d8daa257e405cf07c58e6ed520a2875e7
Vincent Dupont <vincent.touffi@gmail.com>
2013-10-25 11:22:44 +0200

    Move SevenSeg tab to c file instead of header

--- libraries/SevenSeg/SevenSeg.c
+++ libraries/SevenSeg/SevenSeg.c
@@ -3,11 +3,51 @@
  * SevenSeg.cpp
  */
 
-#include <Arduino.h>
+#include "Arduino.h"
 #include "SevenSeg.h"
 #include "SPIc.h"
 
 struct seven_seg Disp;
+
+//  7-seg display
+//   0---
+// 5 |   | 1
+//   6---
+// 4 |   | 2
+//   3---  7.
+// A bit=0 means the led is on (the last one is the dot).
+const unsigned int tab_codes[NB_CHAR] = {
+	0xC0, // 0
+	0xF9, // 1
+	0xA4, // 2
+	0xB0, // 3
+	0x99, // 4
+	0x92, // 5
+	0x82, // 6
+	0xF8, // 7
+	0x80, // 8
+	0x90, // 9
+	0x88, // A
+	0x83, // b
+	0xC6, // C
+	0xA1, // d
+	0x86, // E
+	0x8E, // F
+	0x82, // G (== 6)
+	0x89, // H
+	0xF9, // I (== 1)
+	0xE1, // J
+	0xC7, // L
+	0xC0, // O (== 0)
+	0x8C, // P
+	0x92, // S (== 5)
+	0xC1, // U
+	0x7F, // .
+	0xFF, // space
+	0xBF, // - (CARRET MUST BE THE LAST ONE IN THIS TAB)
+};
+
+const char tab_char[NB_CHAR+1] = "0123456789ABCDEFGHIJLOPSU. -";
 
 static void spiSendAtIndex(unsigned int val, int index);
 static int charToVal(char c);
